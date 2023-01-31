edc7dbc2c26b2d332e1c1d07d38c3ec4dc01ccae
italogguasti <italo.guasti@aluno.ufop.edu.br>
2023-01-31 01:23:55 -0300

    size extensions and styles

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,4 +1,8 @@
+import 'package:dw_delivery_app/app/core/ui/helpers/size_extensions.dart';
+import 'package:dw_delivery_app/app/core/ui/widgets/delivery_button.dart';
 import 'package:flutter/material.dart';
+
+import '../../core/config/env/env.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
@@ -12,9 +16,27 @@ class SplashPage extends StatelessWidget {
       body: Column(
         children: [
           Container(),
-          ElevatedButton(
+          DeliveryButton(
+            label: Env.i['backend_base_url'] ?? '',
             onPressed: () {},
-            child: const Text('Test'),
+            width: 200.0,
+            height: 200.0,
+          ),
+          Text(context.screenHeight.toString()),
+          Text(context.screenWidth.toString()),
+          Row(
+            children: [
+              Container(
+                color: Colors.red,
+                width: context.percentWidth(.5),
+                height: 200.0,
+              ),
+              Container(
+                color: Colors.blue,
+                width: context.percentWidth(.5),
+                height: 200.0,
+              ),
+            ],
           ),
           TextFormField(
             decoration: const InputDecoration(labelText: 'text'),
