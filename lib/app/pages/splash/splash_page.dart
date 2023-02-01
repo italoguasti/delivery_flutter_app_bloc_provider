092322f93f306d30931c55c8a4e69fa46fa3b5fe
italogguasti <italo.guasti@aluno.ufop.edu.br>
2023-02-01 00:39:31 -0300

    splash page

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -2,46 +2,45 @@ import 'package:dw_delivery_app/app/core/ui/helpers/size_extensions.dart';
 import 'package:dw_delivery_app/app/core/ui/widgets/delivery_button.dart';
 import 'package:flutter/material.dart';
 
-import '../../core/config/env/env.dart';
-
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Splash'),
-      ),
-      body: Column(
-        children: [
-          Container(),
-          DeliveryButton(
-            label: Env.i['backend_base_url'] ?? '',
-            onPressed: () {},
-            width: 200.0,
-            height: 200.0,
-          ),
-          Text(context.screenHeight.toString()),
-          Text(context.screenWidth.toString()),
-          Row(
-            children: [
-              Container(
-                color: Colors.red,
-                width: context.percentWidth(.5),
-                height: 200.0,
+      body: ColoredBox(
+        color: const Color(0XFF140E0E),
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                width: context.screenWidth,
+                child: Image.asset(
+                  'assets/images/lanche.png',
+                  fit: BoxFit.cover,
+                ),
               ),
-              Container(
-                color: Colors.blue,
-                width: context.percentWidth(.5),
-                height: 200.0,
+            ),
+            Center(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: context.percentHeight(.30),
+                  ),
+                  Image.asset('assets/images/logo.png'),
+                  const SizedBox(height: 80.0),
+                  DeliveryButton(
+                    label: 'ACESSAR',
+                    onPressed: () {},
+                    width: context.percentWidth(.6),
+                    height: 34.0,
+                  ),
+                ],
               ),
-            ],
-          ),
-          TextFormField(
-            decoration: const InputDecoration(labelText: 'text'),
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
